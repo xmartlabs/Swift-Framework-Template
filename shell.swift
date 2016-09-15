172200ba371f311090a511692e909659388cf749
Mathias Claassen <mathias@xmartlabs.com>
2016-09-15 16:37:27 -0300

    Update shell.swift to Swift 3 Fixed travis

--- shell.swift
+++ shell.swift
@@ -16,132 +16,134 @@ var authorEmail = "swift@xmartlabs.com"
 var userName = "xmartlabs"
 var organizationName = "Xmartlabs SRL"
 
-let fileManager = NSFileManager.defaultManager()
+let fileManager = FileManager.default
 
 let runScriptPathURL = NSURL(fileURLWithPath: fileManager.currentDirectoryPath, isDirectory: true)
-let currentScriptPathURL = NSURL(fileURLWithPath: NSURL(fileURLWithPath: Process.arguments[0], relativeToURL: runScriptPathURL).URLByDeletingLastPathComponent!.path!, isDirectory: true)
-let iOSFrameworkTemplateForlderURL = NSURL(fileURLWithPath: "Framework-iOS", relativeToURL: currentScriptPathURL)
+let currentScriptPathURL = NSURL(fileURLWithPath: NSURL(fileURLWithPath: CommandLine.arguments[0], relativeTo: runScriptPathURL as URL).deletingLastPathComponent!.path, isDirectory: true)
+let iOSFrameworkTemplateForlderURL = NSURL(fileURLWithPath: "Framework-iOS", relativeTo: currentScriptPathURL as URL)
 var newFrameworkFolderPath = ""
 let ignoredFiles = [".DS_Store", "UserInterfaceState.xcuserstate"]
 
 extension NSURL {
-  var fileName: String {
-    var fileName: AnyObject?
-    try! getResourceValue(&fileName, forKey: NSURLNameKey)
-    return fileName as! String
-  }
-
-  var isDirectory: Bool {
-    var isDirectory: AnyObject?
-    try! getResourceValue(&isDirectory, forKey: NSURLIsDirectoryKey)
-    return isDirectory as! Bool
-  }
-
-  func renameIfNeeded() {
-    if let _ = fileName.rangeOfString("XLProductName") {
-      let renamedFileName = fileName.stringByReplacingOccurrencesOfString("XLProductName", withString: frameworkName)
-      try! NSFileManager.defaultManager().moveItemAtURL(self, toURL: NSURL(fileURLWithPath: renamedFileName, relativeToURL: URLByDeletingLastPathComponent))
+    var fileName: String {
+        var fileName: AnyObject?
+        try! getResourceValue(&fileName, forKey: URLResourceKey.nameKey)
+        return fileName as! String
     }
-  }
 
-  func updateContent() {
-    guard let path = path, let content = try? String(contentsOfFile: path, encoding: NSUTF8StringEncoding) else {
-      print("ERROR READING: \(self)")
-      return
+    var isDirectory: Bool {
+        var isDirectory: AnyObject?
+        try! getResourceValue(&isDirectory, forKey: URLResourceKey.isDirectoryKey)
+        return isDirectory as! Bool
     }
-    var newContent = content.stringByReplacingOccurrencesOfString(templateFrameworkName, withString: frameworkName)
-    newContent = newContent.stringByReplacingOccurrencesOfString(templateBundleDomain, withString: bundleDomain)
-    newContent = newContent.stringByReplacingOccurrencesOfString(templateAuthor, withString: author)
-    newContent = newContent.stringByReplacingOccurrencesOfString(templateUserName, withString: userName)
-    newContent = newContent.stringByReplacingOccurrencesOfString(templateAuthorEmail, withString: authorEmail)
-    newContent = newContent.stringByReplacingOccurrencesOfString(templateOrganizationName, withString: organizationName)
-    try! newContent.writeToURL(self, atomically: true, encoding: NSUTF8StringEncoding)
-  }
+
+    func renameIfNeeded() {
+        if let _ = fileName.range(of: "XLProductName") {
+            let renamedFileName = fileName.replacingOccurrences(of: "XLProductName", with: frameworkName)
+            try! FileManager.default.moveItem(at: self as URL, to: NSURL(fileURLWithPath: renamedFileName, relativeTo: deletingLastPathComponent) as URL)
+        }
+    }
+
+    func updateContent() {
+        guard let path = path, let content = try? String(contentsOfFile: path, encoding: String.Encoding.utf8) else {
+            print("ERROR READING: \(self)")
+            return
+        }
+        var newContent = content.replacingOccurrences(of: templateFrameworkName, with: frameworkName)
+        newContent = newContent.replacingOccurrences(of: templateBundleDomain, with: bundleDomain)
+        newContent = newContent.replacingOccurrences(of: templateAuthor, with: author)
+        newContent = newContent.replacingOccurrences(of: templateUserName, with: userName)
+        newContent = newContent.replacingOccurrences(of: templateAuthorEmail, with: authorEmail)
+        newContent = newContent.replacingOccurrences(of: templateOrganizationName, with: organizationName)
+        try! newContent.write(to: self as URL, atomically: true, encoding: String.Encoding.utf8)
+    }
 }
 
 func printInfo<T>(message: T)  {
-  print("\n-------------------Info:-------------------------")
-  print("\(message)")
-  print("--------------------------------------------------\n")
+    print("\n-------------------Info:-------------------------")
+    print("\(message)")
+    print("--------------------------------------------------\n")
 }
 
 func printErrorAndExit<T>(message: T) {
-  print("\n-------------------Error:-------------------------")
-  print("\(message)")
-  print("--------------------------------------------------\n")
-  exit(1)
+    print("\n-------------------Error:-------------------------")
+    print("\(message)")
+    print("--------------------------------------------------\n")
+    exit(1)
 }
 
 func checkThatFrameworkForlderCanBeCreated(frameworkURL: NSURL){
-  var isDirectory: ObjCBool = true
-  if fileManager.fileExistsAtPath(frameworkURL.path!, isDirectory: &isDirectory){
-      printErrorAndExit("\(frameworkName) \(isDirectory.boolValue ? "folder already" : "file") exists in \(runScriptPathURL.path) directory, please delete it and try again")
-  }
+    var isDirectory: ObjCBool = true
+    if fileManager.fileExists(atPath: frameworkURL.path!, isDirectory: &isDirectory){
+        printErrorAndExit(message: "\(frameworkName) \(isDirectory.boolValue ? "folder already" : "file") exists in \(runScriptPathURL.path) directory, please delete it and try again")
+    }
 }
 
 func shell(args: String...) -> (output: String, exitCode: Int32) {
-    let task = NSTask()
+    let task = Process()
     task.launchPath = "/usr/bin/env"
     task.arguments = args
     task.currentDirectoryPath = newFrameworkFolderPath
-    let pipe = NSPipe()
+    let pipe = Pipe()
     task.standardOutput = pipe
     task.launch()
     task.waitUntilExit()
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = NSString(data: data, encoding: NSUTF8StringEncoding) as? String ?? ""
+    let output = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as? String ?? ""
     return (output, task.terminationStatus)
 }
 
 func prompt(message: String, defaultValue: String) -> String {
-  print("\n> \(message) (or press Enter to use \(defaultValue))")
-  let line = readLine()
-  return line == nil || line == "" ? defaultValue : line!
+    print("\n> \(message) (or press Enter to use \(defaultValue))")
+    let line = readLine()
+    return line == nil || line == "" ? defaultValue : line!
 }
 
 print("\nLet's go over some question to create your framework base project!")
 
-frameworkName = prompt("Framework name", defaultValue: frameworkName)
+frameworkName = prompt(message: "Framework name", defaultValue: frameworkName)
 
 // Check if folder already exists
-let newFrameworkFolderURL = NSURL(fileURLWithPath: frameworkName, relativeToURL: runScriptPathURL)
+let newFrameworkFolderURL = NSURL(fileURLWithPath: frameworkName, relativeTo: runScriptPathURL as URL)
 newFrameworkFolderPath = newFrameworkFolderURL.path!
-checkThatFrameworkForlderCanBeCreated(newFrameworkFolderURL)
+checkThatFrameworkForlderCanBeCreated(frameworkURL: newFrameworkFolderURL)
 
-bundleDomain = prompt("Bundle domain", defaultValue: bundleDomain)
-author       = prompt("Author", defaultValue: author)
-authorEmail  = prompt("Author Email", defaultValue: authorEmail)
-userName     = prompt("Github username", defaultValue: userName)
-organizationName = prompt("Organization Name", defaultValue: organizationName)
+bundleDomain = prompt(message: "Bundle domain", defaultValue: bundleDomain)
+author       = prompt(message: "Author", defaultValue: author)
+authorEmail  = prompt(message: "Author Email", defaultValue: authorEmail)
+userName     = prompt(message: "Github username", defaultValue: userName)
+organizationName = prompt(message: "Organization Name", defaultValue: organizationName)
 
 // Copy template folder to a new folder inside run script url called frameworkName
 do {
-  try fileManager.copyItemAtURL(iOSFrameworkTemplateForlderURL, toURL: newFrameworkFolderURL)
+    try fileManager.copyItem(at: iOSFrameworkTemplateForlderURL as URL, to: newFrameworkFolderURL as URL)
 } catch let error as NSError {
-  printErrorAndExit(error.localizedDescription)
+    printErrorAndExit(message: error.localizedDescription)
 }
 
 // rename files and update content
-let enumerator = fileManager.enumeratorAtURL(newFrameworkFolderURL, includingPropertiesForKeys: [NSURLNameKey, NSURLIsDirectoryKey], options: [], errorHandler: nil)!
+let enumerator = fileManager.enumerator(at: newFrameworkFolderURL as URL, includingPropertiesForKeys: [URLResourceKey.nameKey, URLResourceKey.isDirectoryKey], options: [], errorHandler: nil)!
 var frameworkDirectories = [NSURL]()
 print("\nCreating \(frameworkName) ...")
 while let fileURL = enumerator.nextObject() as? NSURL {
     guard !ignoredFiles.contains(fileURL.fileName) else { continue }
     if fileURL.isDirectory {
-      frameworkDirectories.append(fileURL)
+        frameworkDirectories.append(fileURL)
     }
     else {
-      fileURL.updateContent()
-      fileURL.renameIfNeeded()
+        fileURL.updateContent()
+        fileURL.renameIfNeeded()
     }
 }
-for fileURL in frameworkDirectories.reverse() {
-  fileURL.renameIfNeeded()
+for fileURL in frameworkDirectories.reversed() {
+    fileURL.renameIfNeeded()
 }
 
-print(shell("carthage", "update", "--platform", "iOS").output)
-print(shell("git", "init").output)
-print(shell("git", "add", ".").output)
-print(shell("git", "commit", "-m", "'Initial commit'").output)
-print(shell("git", "remote", "add", "origin", "git@github.com:\(userName)/\(frameworkName).git").output)
-print(shell("open", "\(frameworkName).xcworkspace").output)
+print(shell(args: "carthage", "update", "--platform", "iOS").output)
+print(shell(args: "git", "init").output)
+print(shell(args: "git", "add", ".").output)
+print(shell(args: "git", "commit", "-m", "'Initial commit'").output)
+print(shell(args: "git", "remote", "add", "origin", "git@github.com:\(userName)/\(frameworkName).git").output)
+print(shell(args: "open", "Example/\(frameworkName).xcworkspace").output)
+
+
