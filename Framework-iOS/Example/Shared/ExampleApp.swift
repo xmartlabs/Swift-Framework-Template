201116c0f0331b3ed780756b97b8e4948a7743e8
Martin Barreto <martin@xmartlabs.com>
2020-07-17 16:19:58 -0300

    update to SwiftUI

---
+++ Framework-iOS/Example/Shared/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Shared
+//
+//  Copyright © 2020 XLOrganizationName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
